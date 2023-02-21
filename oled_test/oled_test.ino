fa86e01685966248fb54cf653db30111ebc8b835
Aye10032 <aye10032@gmail.com>
2023-02-21 16:45:45 +0800

    LCD模拟点阵显示

--- oled_test/oled_test.ino
+++ oled_test/oled_test.ino
@@ -10,6 +10,8 @@
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+int status[] = {0, 0, 0, 0, 0, 0, 0};
+
 void setup() {
   Serial.begin(9600);
 
@@ -18,19 +20,43 @@ void setup() {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
+  Serial.println("screen init success");
   
-  display.display();
+  init_screen();
   delay(2000); // Pause for 2 seconds
 
-  // Clear the buffer
-  display.clearDisplay();
-  display.display();
-
-  display.fillRect(5, 0, 10, 16, SSD1306_WHITE);
-  display.display();
+  status[1] = 0;
+  status[5] = 0;
+  smal_lcd_show();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  random_show();
+  delay(500);
+}
 
+void init_screen() {
+  for(int i=0; i<7; i++){
+    status[i] = 1;
+  }
+  smal_lcd_show();
+}
+
+void smal_lcd_show(){
+  display.clearDisplay();
+  for(int i=0; i<7; i++){
+    if (status[i] == 1){
+      display.fillRect(i*15, 0, 10, 32, SSD1306_WHITE);
+    }    
+  }
+  display.display();
+}
+
+void random_show(){
+  randomSeed(analogRead(1));  
+  for(int i=0; i<7; i++){
+    status[i] = random(2);
+  }
+  smal_lcd_show();  
 }
